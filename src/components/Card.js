a06b9308b6f935f28d18d9e61a13d3d51ddc3c44
florella-yannis <florellayannis@gmail.com>
2023-10-18 18:52:09 +0200

    composition card + css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,12 @@ import React from 'react';
 // au lieu de taper props on ecrit directement meal
 const Card = ({meal}) => {
     return (
-        <div className="meal-carrd">
+        <div className="meal-card">
 
             <h2>{meal.strMeal}</h2>
             <p> Origin : {meal.strArea}</p>
+            <img src={meal.strMealThumb} alt={"photo " + meal.strMeal} />
+            <p>{meal.strInstructions}</p>
         </div>
     );
 };
